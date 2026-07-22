2a95d75706821f7811fb1300501612cc5090bbc3
zwgknight <166206950@qq.com>
2026-07-22 14:43:42 +0800

    Delete Projective plane 3.0 directory

--- Projective plane 3.0/KnowledgeAndPaperBackground.m
+++
@@ -1,4 +0,0 @@
-%% Related works
-% Tim Penttila and Gordon F. Royle. 1995. Sets of type (m, n) in the affine and projective planes of order nine. Des. Codes Cryptography 6, 3 (Nov. 1995), 229–245. https://doi.org/10.1007/BF01388477
-% Durante, N. (2014). On Sets with Few Intersection Numbers in Finite Projective and Affine Spaces. The Electronic Journal of Combinatorics, 21(4), #P4.13. https://doi.org/10.37236/3434
-% Some work from Tallini scafati, 1966,1972,1976.
